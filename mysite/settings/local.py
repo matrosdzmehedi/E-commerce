ea4483111e1526aeb15afe7f7fe4169a4ee6d59d
mehedi <matrosdzmehedi>
2020-09-11 13:06:23 +0600

    remve password

--- mysite/settings/local.py
+++ mysite/settings/local.py
@@ -4,14 +4,13 @@ from .base import *
 # [ python manage.py runserver --settings=mysite.settings.local ]
 
 
-
-INSTALLED_APPS += [ ]
+INSTALLED_APPS += []
 
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -25,41 +24,32 @@ TEMPLATES = [
 ]
 
 
-
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'E-com',
-        'USER':'postgres',
-        'PASSWORD':'12345',
-        'HOST':'localhost'
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost'
 
     }
 }
 
 
-
-#static section
+# static section
 
 STATIC_URL = '/static/'
-STATIC_DIRS= [os.path.join(BASE_DIR,'static')]
-
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 SITE_ID = 1
 
 
+# email section
 
-
-
-
-#email section
-
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER='dota3hudai@gmail.com'
-EMAIL_HOST_PASSWORD='@ammaremail'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dota3hudai@gmail.com'
+EMAIL_HOST_PASSWORD = ''
